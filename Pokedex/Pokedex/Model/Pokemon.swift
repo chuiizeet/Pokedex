fa8923dd8bf29a303e3f2b99bc1b42dfbb3630c3
chuiizeet <chuy.guerra19@outlook.es>
2019-05-13 11:32:37 -0500

    Pokemon model

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,69 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by imac on 5/13/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class Pokemon {
+    
+    var name: String?
+    var imageUrl: String?
+    var image: UIImage?
+    var id: Int?
+    var weight: Int?
+    var height: Int?
+    var defense: Int?
+    var attack: Int?
+    var description: String?
+    var type: String?
+    var baseExperience: Int?
+    
+    init(id: Int, dictionary: [String: AnyObject]) {
+        
+        self.id = id
+        
+        if let name = dictionary["name"] as? String {
+            self.name = name
+        }
+        
+        if let imageUrl = dictionary["imageUrl"] as? String {
+            self.imageUrl = imageUrl
+        }
+        
+        if let id = dictionary["id"] as? Int {
+            self.id = id
+        }
+        
+        if let weight = dictionary["weight"] as? Int {
+            self.weight = weight
+        }
+        
+        if let height = dictionary["height"] as? Int {
+            self.height = height
+        }
+        
+        if let defense = dictionary["defense"] as? Int {
+            self.defense = defense
+        }
+        
+        if let attack = dictionary["attack"] as? Int {
+            self.attack = attack
+        }
+        
+        if let description = dictionary["description"] as? String {
+            self.description = description
+        }
+        
+        if let type = dictionary["type"] as? String {
+            self.type = type
+        }
+        
+//        if let baseExperience = dictionary["baseExperience"] as? Int {
+//            self.baseExperience = baseExperience
+//        }
+    }
+}
