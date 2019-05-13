1c49d5d0bc3fd1a973aba6db0de11f90bf0eda84
chuiizeet <chuy.guerra19@outlook.es>
2019-05-12 19:28:12 -0500

    Add PokedexCell

--- Pokedex/Pokedex/Controller/PokedexController.swift
+++ Pokedex/Pokedex/Controller/PokedexController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+private let reuseIdentifier = "PokedexCell"
+
 class PokedexController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,10 +36,36 @@ class PokedexController: UICollectionViewController {
         
         navigationController?.navigationBar.barTintColor = .coralRed()
         navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isTranslucent = false
         
         navigationItem.title = "Pokedex"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
         navigationItem.rightBarButtonItem?.tintColor = .white
+        
+        collectionView.register(PokedexCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+}
+
+extension PokedexController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PokedexCell
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
+
+extension PokedexController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 36) / 3
+        return CGSize(width: width, height: width)
+        
     }
 }
---
+++ Pokedex/Pokedex/View/PokedexCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokedexCell.swift
+//  Pokedex
+//
+//  Created by imac on 5/12/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class PokedexCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
